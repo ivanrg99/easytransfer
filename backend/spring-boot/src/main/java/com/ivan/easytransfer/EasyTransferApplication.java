9656a71e19bbfadb92c84ba971ed79ac7f029b9f
ivan <47028390+ivanrg99@users.noreply.github.com>
2023-04-17 18:31:40 +0200

    Add files via upload

---
+++ backend/spring-boot/src/main/java/com/ivan/easytransfer/EasyTransferApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.easytransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyTransferApplication.class, args);
+	}
+
+}
---
+++ backend/spring-boot/src/test/java/com/ivan/easytransfer/EasyTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivan.easytransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
